5d928fba0f22297bf0205867fd33dd8a533fa622
Luuk de Gram <Luukdegram@users.noreply.github.com>
2020-06-16 21:01:32 +0200

    Fix build.zig

--- build.zig
+++ build.zig
@@ -12,8 +12,8 @@ pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
 
     const exe = b.addExecutable("lion", "src/main.zig");
-    exe.addCSourceFile("lib/dr_wav.c", &[_][]const u8{"-std=c99"});
-    exe.addIncludeDir("lib");
+    exe.addCSourceFile("thirdparty/dr_wav.c", &[_][]const u8{"-std=c99"});
+    exe.addIncludeDir("thirdparty");
     exe.linkSystemLibrary("epoxy");
     exe.linkSystemLibrary("openal");
     exe.linkSystemLibrary("glfw");
